7305a6bb1b640b8b68f29b1f99a66385cd861e0a
Andra <94204279+asteele1493@users.noreply.github.com>
2023-04-09 17:37:42 -0700

    UI working

--- src/Components/Categories/index.js
+++ src/Components/Categories/index.js
@@ -1,19 +1,21 @@
 import Products from "../Products";
 import { filteredProducts } from "../../store/products";
 import { useSelector } from "react-redux";
-import Grid from "@mui/material/Grid";
+import  Grid  from "@mui/material/Grid";
 import React from "react";
 
 const Categories = () => {
   const products = useSelector(filteredProducts);
   return (
-      <Grid container spacing={2} xs={3}>
-        {products.map((category) => (
-          <Grid key={category.id}>
-            <Products product={category} />
+<>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {products.map((item) => (
+          <Grid item xs={6} md={4} key={item.name}>
+            <Products product={item} />
           </Grid>
         ))}
       </Grid>
+</>
   );
 };
 
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -3,31 +3,34 @@ import AppBar from "@mui/material/AppBar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import categories from "../../store/categories/index";
 import productSlice from "../../store/products";
 import { useDispatch } from "react-redux";
 
 const Header = () => {
   const dispatch = useDispatch();
-
   return (
-    <AppBar position="static">
-      <Typography role="header" sx={{ display: "flex" }}>
-        Store-o-matic
-      </Typography>
-      <Box sx={{ display: "flex" }}>
-        {categories.map((category) => (
-          <Button
-            key={category.displayName}
-            onClick={() => {
-              dispatch(productSlice.actions.setActiveCategory(category.name));
-            }}
-          >
-            {category.displayName}
-          </Button>
-        ))}
-      </Box>
-    </AppBar>
+    <Box>
+      <AppBar position="static">
+      <Typography variant="h3" sx={{ display: "flex" }} fontFamily ='Arial'>
+            Store-o-matic
+          </Typography>
+        <Toolbar sx={{ justifyContent: "flex-start" }}>
+          <Box>
+            {categories.map((category) => (
+              <Button variant="h3" key={category.displayName}  
+              onClick={() => {dispatch(productSlice.actions.setActiveCategory(category.name))}}>
+                {category.displayName}
+              </Button>
+            ))}
+                <ShoppingCartIcon />
+          </Box>
+      
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
